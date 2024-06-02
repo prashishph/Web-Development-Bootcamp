9d12b41e10e6ceab8437dc6c6bf898aadc3a6ca8
Prashish <prashph456@gmail.com>
2024-06-02 15:55:29 +0545

    refactor: Simplify dice rolling logic and use an array for dice images

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,40 +1,18 @@
-player1 = Math.round(Math.random()*6 + 1);
-player2 = Math.round(Math.random()*6 + 1);
+dice1 = Math.floor(Math.random() * 6) + 1;
+dice2 = Math.floor(Math.random() * 6) + 1;
 
-if (player1 === 1) {
-    document.querySelector(".img1").setAttribute("src","./images/dice1.png");
-}
-else if (player1 === 2) {
-    document.querySelector(".img1").setAttribute("src","./images/dice2.png");
-}
-else if (player1 === 3) {
-    document.querySelector(".img1").setAttribute("src","./images/dice3.png");
-}   
-else if (player1 === 4) {   
-    document.querySelector(".img1").setAttribute("src","./images/dice4.png");
-}
-else if (player1 === 5) {   
-    document.querySelector(".img1").setAttribute("src","./images/dice5.png");
-}
-else {
-    document.querySelector(".img1").setAttribute("src","./images/dice6.png");
+const diceImages = [
+    "./images/dice1.png",
+    "./images/dice2.png",
+    "./images/dice3.png",
+    "./images/dice4.png",
+    "./images/dice5.png",
+    "./images/dice6.png"
+]
+
+function rollDice() {
+    document.querySelector(".img1").setAttribute("src",diceImages[dice1 - 1]);
+    document.querySelector(".img2").setAttribute("src",diceImages[dice2 - 1]);   
 }
 
-if (player2 === 1) {
-    document.querySelector(".img2").setAttribute("src","./images/dice1.png");
-}
-else if (player2 === 2) {
-    document.querySelector(".img2").setAttribute("src","./images/dice2.png");
-}
-else if (player2 === 3) {
-    document.querySelector(".img2").setAttribute("src","./images/dice3.png");
-}   
-else if (player2 === 4) {   
-    document.querySelector(".img2").setAttribute("src","./images/dice4.png");
-}
-else if (player2 === 5) {   
-    document.querySelector(".img2").setAttribute("src","./images/dice5.png");
-}
-else {
-    document.querySelector(".img2").setAttribute("src","./images/dice6.png");
-}
+rollDice();
